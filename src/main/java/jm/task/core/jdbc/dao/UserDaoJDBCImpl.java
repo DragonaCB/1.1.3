801d42a9bced41e51cb5d9bbb6eaadb62b93f5bb
DragonaCB <dragona163@bk.ru>
2025-03-27 19:25:16 +0500

    commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,16 +7,13 @@ import java.sql.*;
 import java.util.*;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Connection connection = null;
 
-    public UserDaoJDBCImpl() {
-        this.connection = Util.getConnection();
-    }
-
+    @Override
     public void createUsersTable() {
         String createTable = "CREATE TABLE IF NOT EXISTS users (Id INT PRIMARY KEY AUTO_INCREMENT, " +
                 "name VARCHAR(255), lastName VARCHAR(255), age INT NOT NULL)";
-        try (PreparedStatement ps = connection.prepareStatement(createTable)) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(createTable)) {
             ps.executeUpdate();
             System.out.println("Database has been created!");
         } catch (SQLException e) {
@@ -24,17 +21,21 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public void dropUsersTable() {
-        try (PreparedStatement ps = connection.prepareStatement("DROP TABLE IF EXISTS users")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement("DROP TABLE IF EXISTS users")) {
             ps.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Connection failed..." + e);
         }
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, name);
             ps.setString(2, lastName);
             ps.setInt(3, age);
@@ -45,8 +46,10 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public void removeUserById(long id) {
-        try (PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE Id = ?")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE Id = ?")) {
             ps.setLong(1, id);
             ps.executeUpdate();
             System.out.println("User " + id + " has been removed!");
@@ -55,9 +58,11 @@ public class UserDaoJDBCImpl implements UserDao {
         }
     }
 
+    @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (PreparedStatement ps = connection.prepareStatement("SELECT * FROM users");
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement("SELECT * FROM users");
              ResultSet resultSet = ps.executeQuery()) {
             while (resultSet.next()) {
                 User user = new User();
@@ -69,13 +74,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 System.out.println("User " + user.getName() + " " + user.getLastName() + " " + user.getAge());
             }
         } catch (SQLException e) {
-            System.out.println("Connection failed..."+e);
+            System.out.println("Connection failed..." + e);
         }
         return users;
     }
 
+    @Override
     public void cleanUsersTable() {
-        try (PreparedStatement ps = connection.prepareStatement("DELETE FROM users")) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ps = connection.prepareStatement("DELETE FROM users")) {
             ps.executeUpdate();
             System.out.println("Users has been cleaned!");
         } catch (SQLException e) {
